2d2e0c50e4c5b467e813b63ec714cd0b754357a7
LuanRodriguesfuturedev <luanrodrigues.softwareengineer@gmail.com>
2023-12-19 22:11:15 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formValido = false;
+
+function validaNome(nomeCompleto){
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >=2;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const transacaoOk = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b> - conta: <b>${numeroContaBeneficiario.value}</b>`;
+
+    formValido = validaNome(nomeBeneficiario.value)
+
+    if (formValido) {
+        const containertransacaoOk =  document.querySelector('.success-message');
+        containertransacaoOk.innerHTML =  transacaoOk;
+        containertransacaoOk.style.display = 'block';
+
+        nomeBeneficiario.value = ' ';
+        numeroContaBeneficiario.value = ' ';
+        valorDeposito.value = ' ';
+
+    }    else {
+        nomeBeneficiario.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    }
+})
+
+nomeBeneficiario.addEventListener('keyup', function(e){
+    console.log(e.target.value);
+    formValido = validaNome(e.target.value);
+
+    if (!formValido) {
+        nomeBeneficiario.classList.add('error');
+        document.querySelector('.error-message').style.display = 'block';
+    }
+    else{
+        nomeBeneficiario.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+    }
+});
